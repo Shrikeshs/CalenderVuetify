7dffc91b80c647a58dd84fff0651c4fd006891a8
SHRIKESH SURESH <shrikeshs98@gmail.com>
2020-10-30 23:40:51 +0530

    Calender showing, Creating methods and firebase integration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import vueTextAreaAutoSize from "vue-textarea-autosize";
+Vue.config.productionTip = false;
+Vue.use(vueTextAreaAutoSize);
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
-
+firebase.initializeApp({
+  apiKey: "AIzaSyDiir8Brpe_5yse2pONJMcQOiYllnqEXXM",
+  authDomain: "calender-vue-b4f1b.firebaseapp.com",
+  databaseURL: "https://calender-vue-b4f1b.firebaseio.com",
+  projectId: "calender-vue-b4f1b",
+  storageBucket: "calender-vue-b4f1b.appspot.com",
+  messagingSenderId: "757669123299",
+  appId: "1:757669123299:web:daa064f0a14637ef4361d6",
+});
+export const db = firebase.firestore();
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
